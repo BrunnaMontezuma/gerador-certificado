f47c8784996538c1f7e51b732173abc38d2611e3
Montezuma <brunnamontezuma18@gmail.com>
2025-10-17 22:50:05 +0100

    Commit da aula: Lista de certificados

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,15 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './_components/navbar/navbar';
 import { CommonModule } from '@angular/common';
+import { PrimaryButton } from "./_components/primary-button/primary-button";
+import { SecondaryButton } from "./_components/secondary-button/secondary-button";
+import { ItemCertificado } from "./_components/item-certificado/item-certificado";
+import { BaseUi } from "./_components/base-ui/base-ui";
+import { Certificados } from "./pages/certificados/certificados";
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, CommonModule],
+  imports: [RouterOutlet, Navbar, CommonModule, PrimaryButton, SecondaryButton, ItemCertificado, BaseUi, Certificados],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
